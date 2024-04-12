b42305e26ca73acaed340ab4aa9558d285b7f250
Chris <Christopherjorge47@icloud.com>
2024-04-12 18:57:10 -0400

    Added Jquery

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+let colorArray = ['green','red','blue','yellow'];
+let pattern = [];
 const nextSequence = () => {
     let number = Math.floor(Math.random() * 4)
-    return number
+    pattern.push(colorArray[number])
 }
 
 console.log(nextSequence())
+console.log(pattern)
+
+$(h1).remove()
