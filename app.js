ba2c0b05355175db57fece100e82012f51b3021a
Chris <Christopherjorge47@icloud.com>
2024-04-12 19:10:25 -0400

    Added switch case to playSequence adding sound

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let colorArray = ['green','red','blue','yellow'];
 let pattern = [];
+let audio = document.createElement('audio')
 const nextSequence = () => {
     let number = Math.floor(Math.random() * 4)
     pattern.push(colorArray[number])
@@ -10,6 +11,27 @@ const playSequence = () =>
     for(let i = 0; i < pattern.length; i++)
     {
         $(`.${pattern[i]}`).fadeOut(150).fadeIn(150)
+        switch (pattern[i])
+        {
+            case 'green':
+                audio.setAttribute('src', './sounds/green.mp3');
+                audio.play();
+                break;
+            case 'red':
+                audio.setAttribute('src', './sounds/red.mp3');
+                audio.play();
+                break;
+            case 'blue':
+                audio.setAttribute('src', './sounds/blue.mp3');
+                audio.play();
+                break;
+            case 'yellow':
+                audio.setAttribute('src', './sounds/yellow.mp3')
+                audio.play();
+                break;
+            default:
+                break;
+        }
     }
 }
 
