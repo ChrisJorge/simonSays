fd712c902e336567c4ba7aee503a981c1882668a
Chris <Christopherjorge47@icloud.com>
2024-04-12 20:59:19 -0400

    Modified playSequence and checkAnswer functions

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ let pattern = [];
 let clickpattern = [];
 let audio = document.createElement('audio');
 let level = 0;
+let clickCount = -1;
 let title = $('.title')[0];
 const nextSequence = () => {
     level ++;
     title.innerHTML = `Level ${level}`
     let number = Math.floor(Math.random() * 4)
     pattern.push(colorArray[number])
+    // playSequence()
+    timer()
 }
 
 const playSound = (sound) => {
@@ -36,16 +39,26 @@ const playSound = (sound) => {
     }
 }
 
-const playSequence = () =>
-{
-    for(let i = 0; i < pattern.length; i++)
-    {
-        $(`.${pattern[i]}`).fadeOut(150).fadeIn(150)
-        playSound(pattern[i])
-        
-    }
-}
 
+let i = 0
+const playSequence = (i) => {
+    $(`.${pattern[i]}`).fadeOut(150).fadeIn(150)
+    playSound(pattern[i])
+} 
+
+const timer = () => {
+    playSequence(i)
+    i += 1
+    setTimeout( () => {
+        if(i < pattern.length)
+        {
+            timer()
+        }
+        else{
+            i = 0;
+        }
+    }, 1000)
+}
 const animate = (color) => {
     $(`.${color}`).addClass('pressed')
 
@@ -54,15 +67,40 @@ const animate = (color) => {
     }, 100)
 }
 
+const checkAnswer = (clicked) => {
+    clickCount ++
+    console.log(`count = ${clickCount}`)
+    // console.log(`pattern  = ${pattern}`)
+    if(clickCount < pattern.length)
+    {
+        if(pattern[clickCount] === clicked)
+        {
+           console.log('correct')
+            // continueCheck(clickCount)
+        }
+        else{
+            console.log('wrong')
+            location.reload()
+        }
+    }
+
+    if (clickCount === pattern.length - 1)
+    {
+        clickCount = -1
+        setTimeout(nextSequence(),3000)
+    }
+    
+    
+}
+
+
 $('.btn').click( (event) => {
     let clicked = event.target.classList[1]
-    clickpattern.push(clicked)
+    console.log(pattern)
     playSound(clicked)
     animate(clicked)
+    checkAnswer(clicked)
 })
-
-
-
 
 
  
